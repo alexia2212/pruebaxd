9cfe0297a9fce86d4149c6b0a5503dae4db5a169
alexia2212 <alexia_jg@outlook.es>
2023-04-18 16:07:48 -0500

    Commit de Jex xd

---
+++ src/main/java/com/example/practicalab3/Practicalab3Application.java
@@ -0,0 +1,13 @@
+package com.example.practicalab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicalab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practicalab3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicalab3/Practicalab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicalab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicalab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
